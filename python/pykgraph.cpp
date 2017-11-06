9ffedf20de660deecb6578ac548af034a2bc86c1
Huayi Zhang <zhanghuayi@megvii.com>
2017-11-06 16:31:24 +0800

    add python3

--- python/pykgraph.cpp
+++ python/pykgraph.cpp
@@ -451,9 +451,20 @@ public:
     }
 };
 
+#if PY_VERSION_HEX >= 0x03000000
+void *
+#else
+void
+#endif
+initialize()
+{
+  import_array();
+}
+
 BOOST_PYTHON_MODULE(pykgraph)
 {
-    import_array();
+    //import_array();
+    initialize();
     python::numeric::array::set_module_and_type("numpy", "ndarray");
     python::class_<KGraph>("KGraph", python::init<PyObject *, string>())
         .def(python::init<>())
--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import subprocess
 from distutils.core import setup, Extension
 
 VERSION = open('version').read().strip()
-GIT_VERSION = subprocess.check_output("git describe --always", shell=True)
+GIT_VERSION = subprocess.check_output("git describe --always", shell=True).decode('utf-8')
 
 pykgraph = Extension('pykgraph',
         language = 'c++',
-        extra_compile_args = ['-O3', '-std=c++11', '-msse2', '-fopenmp', '-DKGRAPH_VERSION=%s' % GIT_VERSION],
+        extra_compile_args = ['-O3', '-std=c++11', '-msse2', '-fopenmp', '-DKGRAPH_VERSION=%s' % GIT_VERSION, '-fPIC', '-DUSE_BLAS=1'],
         extra_link_args = ['-fopenmp'],
         include_dirs = ['.'],
-        libraries = ['boost_python', 'boost_timer'],
+        libraries = ['boost_python-py35', 'boost_timer', 'blas'],
         sources = ['kgraph.cpp', 'metric.cpp', 'python/pykgraph.cpp'],
         depends = ['kgraph.h', 'kgraph-data.h'])
 
